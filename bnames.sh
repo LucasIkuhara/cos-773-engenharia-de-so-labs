40a539610c82ed88255e42371662941b640a73d3
Lucas Ikuhara <lri2911@gmail.com>
2024-09-18 20:53:21 -0300

    chore: update bnames

---
+++ bnames.sh
@@ -0,0 +1,22 @@
+git checkout cow
+git commit --alow-empty -m "chore: checkout to 'cow'"
+git checkout fs
+git commit --alow-empty -m "chore: checkout to 'fs'"
+git checkout lock
+git commit --alow-empty -m "chore: checkout to 'lock'"
+git checkout mmap
+git commit --alow-empty -m "chore: checkout to 'mmap'"
+git checkout net
+git commit --alow-empty -m "chore: checkout to 'net'"
+git checkout pgtbl
+git commit --alow-empty -m "chore: checkout to 'pgtbl'"
+git checkout riscv
+git commit --alow-empty -m "chore: checkout to 'riscv'"
+git checkout syscall
+git commit --alow-empty -m "chore: checkout to 'syscall'"
+git checkout thread
+git commit --alow-empty -m "chore: checkout to 'thread'"
+git checkout traps
+git commit --alow-empty -m "chore: checkout to 'traps'"
+git checkout util
+git commit --alow-empty -m "chore: checkout to 'util'"
